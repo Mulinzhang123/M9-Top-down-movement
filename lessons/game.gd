8f90f6daa9993148ff50ba6ff8ae2d93cbe7b0a6
Mulinzhang123 <MuZhang@s.sfusd.edu>
2025-12-12 13:19:02 -0800

    lesson 13 is done

--- lessons/game.gd
+++ lessons/game.gd
@@ -2,12 +2,13 @@ extends Node2D
 @onready var _count_down: CountDown = %CountDown
 @onready var _runner: Runner = %Runner
 @onready var _finish_line: FinishLine = %FinishLine
+@onready var _bouncer: CharacterBody2D = %Bouncer
 func _ready() -> void:
 	_finish_line.body_entered.connect(func (body: Node) -> void:
 		if body is not Runner:
 			return
 		var runner := body as Runner
-
+	
 		runner.set_physics_process(false)
 		var destination_position := (
 			_finish_line.global_position
@@ -33,7 +34,12 @@ func _ready() -> void:
 		func() -> void:
 			_runner.set_physics_process(true)
 	)
-	
+	_bouncer.set_physics_process(false)
+
+	_count_down.counting_finished.connect(
+		func() -> void:
+			_bouncer.set_physics_process(true)
+	)
 	
 	
 	
